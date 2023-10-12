91ef9c16e70bd790925fdb5656958c53a8cccd66
xinchundou <douxinchun@gmail.com>
2023-10-12 21:43:27 +0800

    Site updated at: 2023-10-12 13:43:27 UTC

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ Original_JEKYLL_ENV=$JEKYLL_ENV
 export JEKYLL_ENV=production
 bundle exec jekyll build
 octopress deploy
-export jeKYLL_ENV=$Original_JEKYLL_ENV
+export JEKYLL_ENV=$Original_JEKYLL_ENV
